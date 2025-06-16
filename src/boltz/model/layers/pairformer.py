fadbc4350fe6cfcf8e527b742c65279f1f4db8da
Simon Levine-Gottreich <sgottreich@absci.com>
2025-06-16 16:26:29 -0700

    Make use_kernels positional arg

--- src/boltz/model/layers/pairformer.py
+++ src/boltz/model/layers/pairformer.py
@@ -325,13 +325,13 @@ class PairformerNoSeqModule(nn.Module):
                     z,
                     pair_mask,
                     chunk_size_tri_attn,
-                    use_kernels=use_kernels,
+                    use_kernels,
                 )
             else:
                 z = layer(
                     z,
                     pair_mask,
                     chunk_size_tri_attn,
-                    use_kernels=use_kernels,
+                    use_kernels,
                 )
         return z
